35bde2b155ca26fa2bc5645f9042077d177a8d57
Hamza Tural <hamzatural14@gmail.com>
2023-03-15 11:27:07 +0300

    test

--- WindowsFormsApp8/Form1.Designer.cs
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace WindowsFormsApp8
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(377, 37);
+            this.button1.Location = new System.Drawing.Point(248, 286);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(74, 129);
             this.button1.TabIndex = 0;
@@ -45,7 +45,7 @@ namespace WindowsFormsApp8
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(433, 316);
+            this.button2.Location = new System.Drawing.Point(369, 305);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(200, 90);
             this.button2.TabIndex = 1;
