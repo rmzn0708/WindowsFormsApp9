b0e43d804cd3feca72207ca8a372085eb7bc9035
rmzn0708 <dnmders0708@gmail.com>
2023-03-15 11:21:57 +0300

    push 2

--- WindowsFormsApp8/Form1.Designer.cs
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -45,7 +45,7 @@ namespace WindowsFormsApp8
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(118, 244);
+            this.button2.Location = new System.Drawing.Point(419, 234);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(200, 90);
             this.button2.TabIndex = 1;
